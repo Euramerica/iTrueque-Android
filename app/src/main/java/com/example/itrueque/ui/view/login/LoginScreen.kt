842ae7008f070095f0c8c4a8ee3b756b2d25e3dd
Samuel <samu6333@gmail.com>
2021-09-25 12:22:20 +0200

    Add login screen.

---
+++ app/src/main/java/com/example/itrueque/ui/view/login/LoginScreen.kt
@@ -0,0 +1,27 @@
+package com.example.itrueque.ui.view.login
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.itrueque.R
+
+@Composable
+fun LoginScreen() {
+    val viewModel: LoginViewModel = hiltViewModel()
+
+    viewModel.loadData()
+    LoginView()
+}
+
+@Composable
+fun LoginView() {
+    Text(text = stringResource(id = R.string.app_name))
+}
+
+@Preview
+@Composable
+fun LoginViewPreview() {
+    LoginView()
+}
