ee5dc9ff37675c483d0a9ef018f4d7d4364e84f8
Samuel <samu6333@gmail.com>
2021-09-27 13:06:08 +0200

    Add color styles.

--- app/src/main/java/com/example/itrueque/ui/theme/Color.kt
+++ app/src/main/java/com/example/itrueque/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.example.itrueque.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Turquoise = Color(0xFF15BFA0)
+val TurquoiseBlack = Color(0xFF2C3A40)
+val TurquoiseWhite= Color(0xFF8A9EA6)
+val GreyLight = Color(0xFFF2F2F2)
+val White = Color(0xFFFFFFFF)
